717d066cc95d346afb9131cc63349d69d2f08195
Prathamesh99 <prathamesh.borhade29@gmail.com>
2019-05-15 22:38:32 +0530

    Subtraction

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 api=Api(app)
 
 def checkposted(posted, function):
-    if(function == "add"):
+    if(function == "add" or function == "subtract"):
         if "x" not in posted or "y" not in posted:
             return 301
         else:
@@ -32,7 +32,28 @@ class Add(Resource):
         }
         return jsonify(ret)
 
-api.add_resource(Add, "/add")
+class Subtract(Resource):
+    def post(self):
+        posted = request.get_json()
+        status = checkposted(posted, "subtract")
+        if(status != 200):
+            retJ = {
+                "Message": "An error happened",
+                "Status code": status
+            }
+            return jsonify(retJ)
+        x = posted["x"]
+        y = posted["y"]
+        x = int(x)
+        y = int(y)
+        diff = x-y
+        ret = {
+            'Message': diff,
+            'Status code': 200
+        }
+        return jsonify(ret)
 
+api.add_resource(Add, "/add")
+api.add_resource(Subtract,"/subtract")
 if __name__ == "__main__":
     app.run(debug=True)
