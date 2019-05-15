b198a4072ede315703ef1da50e12034723030463
Prathamesh99 <prathamesh.borhade29@gmail.com>
2019-05-15 22:15:47 +0530

    Addition

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+from flask_restful import Api, Resource
+app = Flask(__name__)
+
+api=Api(app)
+
+def checkposted(posted, function):
+    if(function == "add"):
+        if "x" not in posted or "y" not in posted:
+            return 301
+        else:
+            return 200
+
+class Add(Resource):
+    def post(self):
+        posted = request.get_json()
+        status = checkposted(posted, "add")
+        if(status != 200):
+            retJ = {
+                "Message": "An error happened",
+                "Status code": status
+            }
+            return jsonify(retJ)
+        x = posted["x"]
+        y = posted["y"]
+        x = int(x)
+        y = int(y)
+        sum = x+y
+        ret = {
+            'Message': sum,
+            'Status code': 200
+        }
+        return jsonify(ret)
+
+api.add_resource(Add, "/add")
+
+if __name__ == "__main__":
+    app.run(debug=True)
